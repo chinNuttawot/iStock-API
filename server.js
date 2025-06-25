a54f14e0ea5fef85170e9fd7c5001b25d3fd3f4a
chin-m1 <znuttawot.s@pttdigital.com>
2025-06-26 00:50:52 +0700

    initial project

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
-const { TestAuth, sendNoti, Login } = require("./src/MainApp")
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.listen(port, () => { console.log(`Listening at http://localhost:${port}`); });
+const port = process.env.PORT || 188;
+const { TestAuth, sendNoti, Login, Logout } = require("./Service/MainApp");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
 
 // post
-app.post("/api/sendnoti", sendNoti)
-app.post("/api/login", Login)
+app.post("/api/sendnoti", sendNoti);
+app.post("/api/login", Login);
+app.post("/api/logout", Logout);
 
 //get
-app.get("/api/testauth", TestAuth)
+app.get("/api/testAuth", TestAuth);
