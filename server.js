04974d9a490786bbcfbd427133125e9212f4450a
chin-m1 <znuttawot.s@pttdigital.com>
2025-08-06 22:31:19 +0700

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 
